c21aa26427ad5359e877793650ac0bdea10f65fd
pantawatb <pantawat.korn@gmail.com>
2025-12-21 17:14:37 +0700

    fix_bugs_change_icon

--- spacecmu/src/components/Sidebar.tsx
+++ spacecmu/src/components/Sidebar.tsx
@@ -78,28 +78,21 @@ export default function Sidebar({ menuItems }: SidebarProps) {
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={2}
+          fill="none"
           stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="w-5 h-5"
         >
-          <rect
-            x="4"
-            y="6"
-            width="16"
-            height="12"
-            rx="2"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-          />
-          <path
-            stroke="currentColor"
-            strokeWidth="2"
-            d="M4 10h16"
-            fill="none"
-          />
+          {/* กระดาษ */}
+          <path d="M6 3h9a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" />
+          {/* เส้นข้อความด้านซ้าย */}
+          <path d="M8 7h5" />
+          <path d="M8 10h5" />
+          <path d="M8 13h5" />
+          <path d="M8 17h8" />
         </svg>
       ),
       link: "/Feeds",
